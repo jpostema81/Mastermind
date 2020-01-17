83374ec08949663bb42db652add2f77b6b927b82
Azoraqua <me@azoraqua.com>
2020-01-17 21:21:38 +0100

    Updated.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-// TODO: Write the logic.
+const btnCheck = $('#btnCheck');
+const btnReset = $('#btnReset');
+const btnRed = $('#btnRed');
+const btnGreen = $('#btnGreen');
+const btnBlue = $('#btnBlue');
+const btnYellow = $('#btnYellow');
+
+$(document).ready(function () {
+    generate();
+
+    btnCheck.on('click', () => check());
+    btnReset.on('click', () => reset());
+});
+
+function generate() {
+    // TODO: Write generate().
+}
+
+function check() {
+    // TODO: Write check().
+}
+
+function reset() {
+    // TODO: Write reset().
+}
+
+const Color = {
+    RED: 1,
+    GREEN: 2,
+    BLUE: 3,
+    YELLOW: 4
+};
